ce67b77bf05dc6c273fc38873d585190069d03d6
SueLee <slee1008@my.bcit.ca>
2022-12-10 01:16:22 -0800

    add report button & attempt on report function

--- comps/Post.js
+++ comps/Post.js
@@ -1,9 +1,43 @@
 
 
-import React, { forwardRef } from 'react';
-import { ChatBubbleOutline, FavoriteBorder, Publish, Repeat, VerifiedUser } from '@mui/icons-material';
+import React, { forwardRef, useState } from 'react';
+import { ChatBubbleOutline, DataObjectOutlined, FavoriteBorder, Publish, Repeat, VerifiedUser } from '@mui/icons-material';
+import styled from 'styled-components';
+import db from '../newfirebase/firebase.config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { getDoc } from 'firebase/firestore';
 
-const Post = forwardRef(({ username, handle, verified, text, image }, ref) => {
+
+const ReportUserButton = styled.div`
+color: white;
+background-color: #F49842;
+border-radius: 10px;
+border: #F49842;
+width: 80px;
+padding: 10px;
+text-align: center;
+font-family: Inter, sans-serif;
+font-size: 12px;
+`
+
+
+const Post = forwardRef(({ username, handle, verified, text, image, key }, ref) => {
+
+    const [report, setReport] = useState("false");
+
+    const reportTweet = e => {
+        setReport("true")
+        db.collection("reported").doc(username.uid,{
+            handle: handle,
+            username: username,
+            text: text,
+            image: image,
+            uid:uid,
+        })
+        db.collection("posts").doc(username.uid).delete()
+           
+    }
+
 
     return (
         <div ref={ref}>
@@ -16,19 +50,20 @@ const Post = forwardRef(({ username, handle, verified, text, image }, ref) => {
                                 {username}{" "}
                                 <span>
                                     {verified && <VerifiedUser />}
-
+                                    <ReportUserButton onClick={reportTweet}>report</ReportUserButton>
                                     <div style={{ fontSize: 12, color: '#777777', fontWeight: 200, }} >@{handle}</div>
                                 </span>
                             </h3>
                         </div>
                         <div>
-                            <p>{text}</p>
+                            <p>{key}</p>
                         </div>
                     </div>
                     <img
                         src={image}
                         alt=""
                     />
+                    
                     <div>
                         <ChatBubbleOutline fontSize='small' />
                         <Repeat fontSize='small' />
